4bbc94d672071126e4786c019c66abb9b4baec33
makakoa <makakoa15@gmail.com>
2015-02-19 00:10:09 -0800

    changed layout

--- app/script.js
+++ app/script.js
@@ -1,4 +1,4 @@
-var intensity = 3;
+var intensity = 0;
 var alphfreq = 'etaoinshrdlcumwfgypbvkjxqz';
 var lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum arcu ut accumsan ornare. Donec ligula purus, suscipit sit amet tellus id, suscipit blandit odio. Donec convallis erat lacus, ac semper neque luctus quis. Donec in dolor venenatis, porta dolor at, sagittis nibh. Duis vel interdum libero. Donec a tellus commodo, rutrum enim at, finibus ante. Quisque ipsum odio, laoreet euismod orci eu, efficitur porttitor urna. Etiam erat mi, accumsan molestie commodo eu, pulvinar at lorem. Nullam vulputate risus a ex commodo aliquet. Quisque at leo sem. Nam et turpis vel ex pulvinar finibus. Maecenas semper mi quis rutrum pulvinar. Fusce sit amet justo placerat massa cursus bibendum. Vivamus vitae massa blandit, ultricies enim quis, imperdiet nisi. In nulla enim, ultricies quis scelerisque sed, lacinia fringilla leo. Maecenas interdum, lacus eu laoreet rutrum, purus sapien tempus nisi, a dictum erat lectus at felis. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus ultrices viverra. Integer sodales eleifend enim, iaculis tempor erat finibus id.';
 var training = false;
@@ -75,7 +75,7 @@ $(document).ready(function() {
       if (n == textarray.length || training == false) return;
       canvas.clearRect(0, 0, 500, 500);
       console.log('printing ' + textarray[n]);
-      if(textarray[n]) base.draw(canvas, textarray[n], 0, 100, 72);
+      if(textarray[n]) base.draw(canvas, textarray[n], 0, 100, 120);
       train(textarray, ++n)
       }, speed);
     }
