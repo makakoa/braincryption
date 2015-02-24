11b89456f87e5383209a23fe0b23a2815ab0a981
makakoa <makakoa15@gmail.com>
2015-02-24 13:54:58 -0800

    add about

--- app/script.js
+++ app/script.js
@@ -3,6 +3,7 @@
 var intensity = 0;
 var alphfreq = 'etaoinshrdlcumwfgypbvkjxqz';
 var lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum arcu ut accumsan ornare. Donec ligula purus, suscipit sit amet tellus id, suscipit blandit odio. Donec convallis erat lacus, ac semper neque luctus quis. Donec in dolor venenatis, porta dolor at, sagittis nibh. Duis vel interdum libero. Donec a tellus commodo, rutrum enim at, finibus ante. Quisque ipsum odio, laoreet euismod orci eu, efficitur porttitor urna. Etiam erat mi, accumsan molestie commodo eu, pulvinar at lorem. Nullam vulputate risus a ex commodo aliquet. Quisque at leo sem. Nam et turpis vel ex pulvinar finibus. Maecenas semper mi quis rutrum pulvinar. Fusce sit amet justo placerat massa cursus bibendum. Vivamus vitae massa blandit, ultricies enim quis, imperdiet nisi. In nulla enim, ultricies quis scelerisque sed, lacinia fringilla leo. Maecenas interdum, lacus eu laoreet rutrum, purus sapien tempus nisi, a dictum erat lectus at felis. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus ultrices viverra. Integer sodales eleifend enim, iaculis tempor erat finibus id.';
+var about = 'Visual encryption experiment utilizing neural adaptation to visually encode information. By progressivly disfiguring a font while a user reads it, braincryption aims to subconsciously teach a user to recognize a new subset of characters. By doing this braincryption attempts to link new characters to existing pattern recognizers, rather than teach a new language or subset. With succesful braincryption, a user could theoretically read these characters as if they were standard english characters while the actual characters are unrecognizable.';
 var training = false;
 var speed = 300;
 var base, v1, v2, v3;
@@ -77,7 +78,13 @@ $(document).ready(function() {
   speedSlider.addEventListener('change', changeSpeed, false);
 
   $('#input').text(lorem);
+
+  $('#about').click(function() {
+    $('#input').text(about);
+  });
+
   $('#braincrypt').click(function() {
+    if(training) return training = false;
     training = true;
     renderFont();
     var text = $('#input').val();
@@ -89,7 +96,7 @@ $(document).ready(function() {
       if (n == textarray.length || training == false) return;
       canvas.clearRect(0, 0, 500, 500);
       console.log('printing ' + textarray[n]);
-      if(textarray[n]) base.draw(canvas, textarray[n], 0, 100, 120);
+      if(textarray[n]) base.draw(canvas, textarray[n], 0, 100, 64);
       train(textarray, ++n)
       }, speed);
     }
