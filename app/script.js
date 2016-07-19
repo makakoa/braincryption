859ee7b90e0e0a3943229ff05ca14a98e25c9558
Cameron Yee <makakoa57@gmail.com>
2016-07-19 14:05:49 -0700

    fix intensity algorithm

--- app/script.js
+++ app/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var about = 'Visual encryption experiment utilizing neural adaptation to visually encode information. By progressively disfiguring a font while a user reads it, braincryption aims to subconsciously teach a user to recognize a new subset of characters. The idea is based off of the principles of pattern recognizers and neural adaptation in attempt to link warped characters to existing pattern recognizers for the original character. This is in contrast with teaching a new language, cryptography secret, or new subset of characters. The idea is not unlike how anyone can read their own bad hand-writing while others have trouble. With successful braincryption, a user could theoretically read these characters as if they were standard english characters while the actual characters are unrecognizable to the general population. This technology is currently experimental but with success could be applied to real world situations where sensitive information may be digitally encrypted but still vulnerable to over-the-shoulder screen readers. Ultimately a braincrypted font would be able to be downloaded by the client and applied to any device or platform. If you would like to know more please feel free to contact me. Otherwise I encourage you to play around with this and try it out. Paste your own text here or read this wonderful paragraph as many times as you like. To start hit the braincryption button below. The intensity and speed of the trainer can be modified at any time by using the sliders provided. As a side note, the app and fonts are still under development. Any feedback on certain characters is appreciated. Eventually this training process will be formatted as a paragraph instead of single words. As a disclaimer, this is experimental, so use at your own risk! Hope you enjoy!';
+var alphfreq = 'etaoinshrdlcumwfgypbvkjxqz';
 
 var inputBox = document.getElementById('input');
 var braincryptedDiv = document.getElementById('braincrypted');
@@ -8,24 +9,67 @@ var braincryptedDiv = document.getElementById('braincrypted');
 var text = about;
 inputBox.value = about;
 var intensity = 0;
+var legend = {};
 
-renderTextAtIntensity(text, intensity);
+render();
 
+// Rendering
 function render() {
-  renderTextAtIntensity(text, intensity);
+  updateLegend(intensity);
+  renderText(text);
 }
-function renderTextAtIntensity(text, i) {
+
+function renderText(text) {
+  var letters = text.split('');
   var result = '';
-  result += [
-    '<span class="',
-    ['one', 'two', 'three'][i%3],
-    '">',
-    text,
-    ' </span>'
-  ].join('');
+  var tmp = '';
+  var currentLevel = 'one';
+  letters.forEach(function(letter) {
+    var l = legend[letter];
+    if (letter === ' ' || l === currentLevel) {
+      tmp += letter;
+    } else {
+      result += encryptChunk(tmp, currentLevel);
+      tmp = letter;
+      currentLevel = l;
+    }
+    // result += encryptChunk(letter, legend[letter]);
+  });
   braincryptedDiv.innerHTML = result;
 }
+function encryptChunk(text, level) {
+  return [
+    '<span class="',
+    level || 'one',
+    '">',
+    text,
+    '</span>'
+  ].join('');
+}
 
+// Legend generation
+function updateLegend(i) { // i = intensity
+  var levels = ['one', 'two', 'three'];
+  var sets = {};
+  if (i < 26) {
+    sets.one = alphfreq.slice(i%26);
+    sets.two = alphfreq.slice(0, i%26);
+    sets.three = '';
+  } else {
+    sets.one = '';
+    sets.two = alphfreq.slice(0, i%26);
+    sets.three = alphfreq.slice(i%26);
+  }
+  levels.forEach(function(level) {
+    sets[level].split('').forEach(function(letter) {
+      legend[letter] = level;
+    });
+  });
+}
+
+
+
+// Controls
 inputBox.addEventListener('input', textChange, false);
 inputBox.addEventListener('change', textChange, false);
 function textChange(e) {
@@ -38,7 +82,7 @@ intensitySlider.addEventListener('input', intensityChange);
 intensitySlider.addEventListener('change', intensityChange);
 function intensityChange(e) {
   intensity = parseInt(e.target.value);
-  renderTextAtIntensity(text, intensity);
+  render();
 }
 
 var aboutButton = document.getElementById('about');
