391d28e9810cfa90641289a84ef28470f46d7682
Cameron Yee <makakoa57@gmail.com>
2016-09-11 12:48:51 -0700

    persisting state works across all tabs in window

--- extension/src/browser_action/scripts.js
+++ extension/src/browser_action/scripts.js
@@ -1,12 +1,49 @@
 'use strict';
+/* globals chrome */
 
-var encryptButton = document.getElementById('encrypt');
-encryptButton.addEventListener('click', encrypt);
-function encrypt() {
-  console.log('encrypting');
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {message: 'encrypt'}, function(response) {
-      console.log(response);
-    });
+var settings = {
+  intensity: 1,
+  enabled: false
+};
+chrome.storage.local.get('braincryption', function(res) {
+  settings = res.braincryption || settings;
+  updateSettings();
+});
+
+var intensitySlider = document.getElementById('intensity');
+intensitySlider.value = settings.intensity;
+intensitySlider.addEventListener('input', intensityChange);
+intensitySlider.addEventListener('change', intensityChange);
+function intensityChange(e) {
+  settings.intensity = parseInt(e.target.value);
+  console.log('Intensity change: ', settings.intensity);
+  updateSettings();
+}
+
+var enableButton = document.getElementById('enable');
+enableButton.innerText = settings.enabled ? 'Enabled' : 'Disabled';
+enableButton.className = settings.enabled ? 'enabled' : '';
+enableButton.addEventListener('click', toggleEnable);
+function toggleEnable() {
+  settings.enabled = !settings.enabled;
+  updateSettings();
+}
+
+function updateSettings() {
+  enableButton.innerText = settings.enabled ? 'Enabled' : 'Disabled';
+  enableButton.className = settings.enabled ? 'enabled' : '';
+
+  intensitySlider.value = settings.intensity;
+
+  // Store
+  chrome.storage.local.set({braincryption: settings}, function() {
+    console.log('Updated Settings: ', settings);
+  });
+
+  // Send to page
+  chrome.tabs.getAllInWindow(null, function(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+      chrome.tabs.sendMessage(tabs[i].id, 'SETTINGS_CHANGED');
+    }
   });
 }
--- extension/src/inject/scripts.js
+++ extension/src/inject/scripts.js
@@ -1,10 +1,35 @@
 'use strict';
+/* globals chrome */
 
 console.log('braincyption running');
 
-var body = document.getElementsByTagName('body')[0];
+var braincryptionSettings;
+function getSettings() {
+  chrome.storage.local.get('braincryption', function(res) {
+    console.log('Settings: ', res);
+    braincryptionSettings = res.braincryption || braincryptionSettings;
+    updatePage();
+  });
+}
+getSettings(); // Get initial settings
 
-chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
-  if (req.message === 'encrypt') body.classList.toggle('braincrypt');
-  sendResponse('encrypting');
+// Settings Changed
+chrome.runtime.onMessage.addListener(function(req) {
+  if (req === 'SETTINGS_CHANGED') getSettings();
 });
+
+var body = document.getElementsByTagName('body')[0];
+var currentLevel = '';
+function updatePage() {
+  if (!braincryptionSettings) return;
+  console.log('braincryption: ', braincryptionSettings);
+
+  if (braincryptionSettings.enabled) {
+    body.classList.remove('braincrypt' + currentLevel);
+    body.classList.add('braincrypt' + braincryptionSettings.intensity);
+    currentLevel = braincryptionSettings.intensity;
+  } else {
+    body.classList.remove('braincrypt' + currentLevel);
+    currentLevel = '';
+  }
+}
